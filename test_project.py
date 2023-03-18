dc0785c4a90a97a7909c378e79a4753359f77432
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2023-03-18 20:51:42 +0200

    added more test

--- test_project.py
+++ test_project.py
@@ -1,40 +1,71 @@
 import os
-from project import find_duplicates, remove_duplicates
+import filecmp
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+from main import get_files, get_duplicates, remove_duplicates
 
 
-def test_find_duplicates(tmp_path):
-    """Test that find_duplicates returns the correct list of duplicate files."""
-    # create some files with duplicate sizes
-    path = str(tmp_path)
-    with open(os.path.join(path, "file1.txt"), "w") as f:
-        f.write("Hello, world!")
-    with open(os.path.join(path, "file2.txt"), "w") as f:
-        f.write("Goodbye, world!")
-    with open(os.path.join(path, "file3.txt"), "w") as f:
-        f.write("Hello, world!")
-    # call find_duplicates and check the result
-    files = [os.path.join(path, f) for f in os.listdir(path)]
-    duplicates = find_duplicates(files)
-    assert len(duplicates) == 1
-    assert len(duplicates[0]) == 2
-    assert os.path.basename(duplicates[0][0]) == "file1.txt"
-    assert os.path.basename(duplicates[0][1]) == "file3.txt"
+def test_get_files():
+    with TemporaryDirectory() as tmpdir:
+        os.makedirs(os.path.join(tmpdir, "subdir1"))
+        os.makedirs(os.path.join(tmpdir, "subdir2"))
+        with open(os.path.join(tmpdir, "file1.txt"), "w") as f:
+            f.write("test")
+        with open(os.path.join(tmpdir, "subdir1", "file2.txt"), "w") as f:
+            f.write("test")
+        with open(os.path.join(tmpdir, "subdir2", "file3.png"), "w") as f:
+            f.write("test")
+        files = get_files(tmpdir, ".txt")
+        assert len(files) == 2
+        assert os.path.join(tmpdir, "file1.txt") in files
+        assert os.path.join(tmpdir, "subdir1", "file2.txt") in files
 
 
-def test_remove_duplicates(tmp_path):
-    """Test that remove_duplicates removes the correct files."""
-    # create some files with duplicate sizes
-    path = str(tmp_path)
-    with open(os.path.join(path, "file1.txt"), "w") as f:
-        f.write("Hello, world!")
-    with open(os.path.join(path, "file2.txt"), "w") as f:
-        f.write("Goodbye, world!")
-    with open(os.path.join(path, "file3.txt"), "w") as f:
-        f.write("Hello, world!")
-    # call find_duplicates and remove_duplicates and check the result
-    files = [os.path.join(path, f) for f in os.listdir(path)]
-    duplicates = find_duplicates(files)
-    remove_duplicates(duplicates)
-    assert len(os.listdir(path)) == 2
-    assert "file1.txt" in os.listdir(path)
-    assert "file2.txt" in os.listdir(path)
+def test_get_duplicates():
+    with TemporaryDirectory() as tmpdir:
+        with open(os.path.join(tmpdir, "file1.txt"), "w") as f1, open(
+            os.path.join(tmpdir, "file2.txt"), "w"
+        ) as f2, open(os.path.join(tmpdir, "file3.txt"), "w") as f3:
+            f1.write("test")
+            f2.write("test")
+            f3.write("test2")
+        files = [
+            os.path.join(tmpdir, "file1.txt"),
+            os.path.join(tmpdir, "file2.txt"),
+            os.path.join(tmpdir, "file3.txt"),
+        ]
+        duplicates = get_duplicates(files)
+        assert len(duplicates) == 1
+        assert os.path.getsize(os.path.join(tmpdir, "file1.txt")) in duplicates
+        assert (
+            os.path.join(tmpdir, "file1.txt")
+            in duplicates[os.path.getsize(os.path.join(tmpdir, "file1.txt"))]
+        )
+        assert (
+            os.path.join(tmpdir, "file2.txt")
+            in duplicates[os.path.getsize(os.path.join(tmpdir, "file1.txt"))]
+        )
+
+
+def test_remove_duplicates():
+    with TemporaryDirectory() as tmpdir:
+        with open(os.path.join(tmpdir, "file1.txt"), "w") as f1, open(
+            os.path.join(tmpdir, "file2.txt"), "w"
+        ) as f2, open(os.path.join(tmpdir, "file3.txt"), "w") as f3:
+            f1.write("test")
+            f2.write("test")
+            f3.write("test2")
+        files = [
+            os.path.join(tmpdir, "file1.txt"),
+            os.path.join(tmpdir, "file2.txt"),
+            os.path.join(tmpdir, "file3.txt"),
+        ]
+        duplicates = get_duplicates(files)
+        with patch("os.remove") as mock_remove:
+            remove_duplicates(duplicates)
+            assert mock_remove.call_count == 1
+            assert mock_remove.call_args_list[0][0][0] in [
+                os.path.join(tmpdir, "file2.txt"),
+                os.path.join(tmpdir, "file1.txt")
+            ]
+
