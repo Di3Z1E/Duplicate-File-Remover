b639f0b7809afa50953c7a13e3790b4900957668
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2023-03-18 20:47:39 +0200

    added filecmp and some functionally to the project
    Added the support of reading txt files to locate duplicated / similar txt files

--- project.py
+++ project.py
@@ -1,50 +1,79 @@
 import os
+import filecmp
 
 
-def get_files(path):
-    """Returns a list of all files in a directory."""
+def main():
+    """
+    The main function of the program.
+    """
+    path = input("Enter directory path: ")
+    while not os.path.isdir(path):
+        path = input("Invalid directory path. Enter a valid path: ")
+    file_type = input("Enter file type (e.g. .txt): ")
+    files = get_files(path, file_type)
+    duplicates = get_duplicates(files)
+    if not duplicates:
+        print("No duplicate files found.")
+    else:
+        print("Duplicate files found:")
+        for size, paths in duplicates.items():
+            print(f"{size} bytes:")
+            for path in paths:
+                print(f"- {path}")
+        choice = input("Do you want to remove the duplicate files? (y/n) ")
+        while choice.lower() not in ["y", "n"]:
+            choice = input(
+                "Invalid choice. Do you want to remove the duplicate files? (y/n) "
+            )
+        if choice.lower() == "y":
+            remove_duplicates(duplicates)
+            print("Duplicate files removed.")
+        else:
+            print("Duplicate files not removed.")
+
+
+def get_files(path, file_type):
+    """
+    Returns a list of all files in the given directory and its subdirectories that match the given file type.
+    """
     files = []
     for dirpath, dirnames, filenames in os.walk(path):
         for filename in filenames:
-            full_path = os.path.join(dirpath, filename)
-            files.append(full_path)
+            if filename.endswith(file_type):
+                files.append(os.path.join(dirpath, filename))
     return files
 
 
-def find_duplicates(files):
-    """Returns a list of all files with duplicate sizes."""
-    sizes = {}
-    for file in files:
+def get_duplicates(files):
+    """
+    Returns a dictionary of all duplicate files in the given list of files, grouped by size.
+    """
+    duplicates = {}
+    total_files = len(files)
+    for i, file in enumerate(files):
         size = os.path.getsize(file)
-        if size not in sizes:
-            sizes[size] = [file]
+        if size in duplicates:
+            for existing_file in duplicates[size]:
+                if filecmp.cmp(file, existing_file):
+                    duplicates[size].append(file)
+                    break
+            else:
+                duplicates[size].append(file)
         else:
-            sizes[size].append(file)
-    return [group for group in sizes.values() if len(group) > 1]
+            duplicates[size] = [file]
+        print(f"Processed {i+1} of {total_files} files.", end="\r")
+    print()  # print a newline character to move the cursor to the next line
+    return {size: paths for size, paths in duplicates.items() if len(paths) > 1}
 
 
 def remove_duplicates(duplicates):
-    """Removes duplicate files."""
-    for group in duplicates:
-        for file in group[1:]:
-            os.remove(file)
+    """
+    Removes all duplicate files in the given dictionary of duplicates, except for the first one in each group.
+    """
+    for size, paths in duplicates.items():
+        for path in paths[1:]:
+            os.remove(path)
 
 
 if __name__ == "__main__":
-    path = input("Enter the path to the directory to search: ")
-    files = get_files(path)
-    duplicates = find_duplicates(files)
-    if duplicates:
-        print("Duplicate files found:")
-        for group in duplicates:
-            print("-" * 40)
-            for file in group:
-                print(file)
-        response = input("Do you want to delete the duplicate files? (y/n) ")
-        if response.lower() == "y":
-            remove_duplicates(duplicates)
-            print("Duplicate files deleted.")
-        else:
-            print("No files were deleted.")
-    else:
-        print("No duplicate files found.")
+    main()
